8578837251b17fb1953d9a95096e7d57a8e46f41
naomifukuta <itismenaomi09@gmail.com>
2024-03-20 21:31:40 +0900

    styling flash message using alpine.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
    {{-- blade-formatter-disable --}}
   <style type="text/tailwindcss">
     .btn {
@@ -34,16 +35,29 @@
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
     
     <h1 class="text-2xl mb-4"> @yield('title')</h1>
-    <div>
-        {{-- @if(session()->has('success')) --}}
-        {{-- <div>{{ session('success') }}</div> --}}
-        <div class="mb-10 rounded border border-green-400 bg-green px-4 py-3 text-lg text-green-700 ">
-            <strong class="font-bold">Success!</strong>
-            <div> This is a flash message</div>
+    {{-- apine has custom attributes like x-data , x-data はALPINEの新しいコンポーネントを定義する--}}
+    <div x-data="{ flash: true}">
+        @if(session()->has('success'))
         
+        <div x-show="flash" 
+        class=" relative mb-10 rounded border border-green-400 bg-green px-4 py-3 text-lg text-green-700 "  role="alert">
+            <strong class="font-bold">Success!</strong>
+            <div>{{ session('success') }}</div>
+        
+
+            {{-- //svg element that renders a "X" sign  --}}
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                  stroke-width="1.5" @click="flash = false"
+                  stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </span>
+
+
         </div>
 
-        {{-- @endif --}}
+        @endif
     </div>
     <div>
         @yield('content')  
