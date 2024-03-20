a76692cdcf85e529ac0878d503d079f89802d0b9
naomifukuta <itismenaomi09@gmail.com>
2024-03-20 19:33:00 +0900

    adding Styling with Tailwind

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,15 +3,17 @@
 @section('title', 'The list of tasks')
 
 @section('content')
-<div>
+<nav class ="mb-4">
     
-    <a href="{{route('tasks.create')}}">Add Task</a>
-</div>
+    <a href="{{route('tasks.create')}}" class="font-medium text-gray-700 underline decoration-pink-500">Add Task</a>
+</nav>
     {{-- display all the list of task  --}}
     @forelse ($tasks as $task)  
         <div>
             {{-- if you named routes,you only need to use route function,you specify the route name  --}}
-            <a href="{{ route('tasks.show' ,['task' => $task->id]) }}" >{{$task->title}}</a>
+            <a href="{{ route('tasks.show' ,['task' => $task->id]) }}"
+                @class(['font-bold','line-through' => $task->completed]) >{{$task->title}}
+            </a>
         </div>    
     @empty
         <div>There are no tasks!</div>
@@ -19,7 +21,7 @@
 
 {{-- コレクションの要素数を確認し、その数が 0 より大きい場合にのみページネーションリンクを表示します。 --}}
     @if ($task->count())
-    <nav>
+    <nav class="mb-4">
         {{ $tasks->links() }}      
     </nav>  
     @else
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Task list App</title>
     @yield('styles')
 </head>
-<body>
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
     
-    <h1> @yield('title')</h1>
+    <h1 class="text-2xl mb-4"> @yield('title')</h1>
     <div>
         @if(session()->has('success'))
         <div>{{ session('success') }}</div>
